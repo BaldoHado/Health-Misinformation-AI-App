63bd14b3c14c25040fe32ada76e04fab5c17ef09
ZealSV <54489933+ZealSV@users.noreply.github.com>
2024-04-13 15:52:47 -0700

    added profile + routing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PromptAIPage from "./pages/PromptAIPage";
+import Profile from "./pages/ProfilePage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prompt-ai" element={<PromptAIPage />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
