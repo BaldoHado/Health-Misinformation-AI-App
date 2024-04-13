d7339da16c78791818702ef8832e68c22fcbcfa3
Adam Ta <adamta2004@gmail.com>
2024-04-13 14:31:44 -0700

    fixed header, removed mui button

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import styles from "./Header.module.scss";
-import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className={styles.header}>
       <div className={styles.headerCenter}>
-        <Button className={styles.navBtn}>
+        <div className={styles.navBtn}>
           <Link to="/">Dashboard</Link>
-        </Button>
-        <Button className={styles.navBtn}>
+        </div>
+        <div className={styles.navBtn}>
           <Link to="/prompt-ai">Prompt AI</Link>
-        </Button>
-        <Button className={styles.navBtn}>
+        </div>
+        <div className={styles.navBtn}>
           <Link to="/profile">Profile</Link>
-        </Button>
+        </div>
       </div>
     </nav>
   );
