e0a4e35dc85254f91e908711a6b5e02ab0000bb1
Adam Ta <adamta2004@gmail.com>
2024-04-13 12:01:44 -0700

    components

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <h1>
-        Hello
-    </h1>
+    <div>
+      <h1>Hello</h1>
+    </div>
   );
-}; 
+};
 
 export default Home;
