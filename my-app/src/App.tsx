6fb2f53a376695b3e51e89c9bbaa6683d03cfde5
PhysBoom <physboomtechdevelopment@gmail.com>
2024-04-14 13:37:21 -0700

    BR --> HR

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import PromptAIPage from "./pages/PromptAIPage";
 import Profile from "./pages/ProfilePage";
 import Admin from "./pages/AdminPortalPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prompt-ai" element={<PromptAIPage />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
